b5adeef80d5d767f2b3fc50d55ba49f088fb4359
leopii98 <rippergames98@gmail.com>
2026-01-29 21:16:08 +0100

    add-API

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-import { useState, useEffect } from 'react'
 import axios from 'axios'
+import { useState } from 'react'
 
 
 function App() {
+  const apiUrl = "https://lanciweb.github.io/demo/api/actors/,https://lanciweb.github.io/demo/api/actresses/,"
   //const [count, setCount] = useState(0)
+  axios.get(apiUrl).then((res) => {
+    console.log(res);
+  })
 
   return (
     <div><h1>Hello React API</h1></div>
