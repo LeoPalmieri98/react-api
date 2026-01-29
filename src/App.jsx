8774b80685f19aad54f392bd417a7f895afb1a39
leopii98 <rippergames98@gmail.com>
2026-01-29 21:09:14 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+
+function App() {
+  //const [count, setCount] = useState(0)
+
+  return (
+    <div><h1>Hello React API</h1></div>
+  )
+}
+
+export default App
