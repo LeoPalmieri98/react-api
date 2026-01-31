04ba6e471d325cde917b9883d6f1b8491e211e1e
leopii98 <rippergames98@gmail.com>
2026-01-31 12:08:53 +0100

    fix.API,add.card.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,59 @@
 import axios from 'axios'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 
 function App() {
-  const apiUrl = "https://lanciweb.github.io/demo/api/actors/,https://lanciweb.github.io/demo/api/actresses/,"
-  //const [count, setCount] = useState(0)
-  axios.get(apiUrl).then((res) => {
-    console.log(res);
-  })
 
+  const [actors, setActors] = useState([]);
+
+  function mounted() {
+    const apiUrl = "https://lanciweb.github.io/demo/api/actors/";
+    axios.get(apiUrl).then((res) => {
+
+      setActors(res.data);
+      console.log(res.data);
+
+    }).catch(error => {
+      console.log("Ops,abbiamo un problema:", error.message);
+    })
+  }
+  useEffect(mounted, []);
   return (
-    <div><h1>Hello React API</h1></div>
+    <div className="container mt-5">
+      <h1 className="text-center mb-4">Hollywood Stars</h1>
+
+      <div className="d-flex flex-wrap gap-3 justify-content-center">
+        {actors.map((person, index) => (
+          <div className="card card-size" key={index}>
+            <img
+              src={person.image}
+              className="card-img-top"
+              alt={person.name}
+            />
+            <h5 className="card-text">
+              {person.name}
+            </h5>
+            <p className="card-text">
+              {person.birth_year}
+            </p>
+            <p className="card-text">
+              {person.nationality}
+            </p>
+            <p className="card-text">
+              {person.biography
+              }
+            </p>
+            <p className="card-text">
+              {person.known_for
+
+              }
+            </p>
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
 
 export default App
+
